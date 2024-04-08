a560f560a8356303ff09b117a7f0e26764113a2f
Caio Espindola <caioespindola@hotmail.com>
2024-04-08 09:57:15 -0300

    improving dashboard part2

--- app/components/Dashboard/styles.ts
+++ app/components/Dashboard/styles.ts
@@ -1,13 +1,95 @@
 import { orange } from '@mui/material/colors';
 
 const styles = {
-  textCard: {
+  cardsContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  cardsBoxCol1: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginRight: '30px',
+    /* alignItems: 'center', */
+    /* width: '280px', */
+    '@media (max-width: 1000px)': {
+      marginRight: '0', // Removendo margem direita
+      // Definindo a largura como 100% para ocupar toda a largura da tela
+    },
+    '@media (max-width: 600px)': {
+      width: '100%', // Definindo a largura como 100% para ocupar toda a largura da tela
+    },
+  },
+  cards: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    width: '280px',
+    backgroundColor: 'transparent',
+    marginBottom: '20px',
+    '@media (max-width: 600px)': {
+      // Estilos para telas menores que 600px
+      width: '100%', // Definindo a largura como 100% para ocupar toda a largura da tela
+    },
+  },
+  cardsContent: {
+    backgroundColor: 'transparent',
+    border: '1px solid white',
+    width: '280px',
+    '@media (max-width: 600px)': {
+      // Estilos para telas menores que 600px
+      width: '100%', // Definindo a largura como 100% para ocupar toda a largura da tela
+    },
+  },
+  cardsText: {
     textAlign: 'center',
     span: {
       color: orange[800],
     },
   },
-  textAccordion: {
+  cardGraphicContainer: {
+    width: '300px',
+    height: '300px',
+    '@media (max-width: 1000px)': {
+      marginBottom: '20px',
+    },
+    '@media (max-width: 600px)': {
+      // Estilos para telas menores que 600px
+      width: '100%', // Definindo a largura como 100% para ocupar toda a largura da tela
+    },
+  },
+  cardGraphic: {
+    borderRadius: 4,
+  },
+
+  CardGraphicContent: {
+    backgroundColor: '#2A2E30',
+    border: '1px solid white',
+    width: '300px',
+    height: '300px',
+    '@media (max-width: 600px)': {
+      // Estilos para telas menores que 600px
+      width: '100%', // Definindo a largura como 100% para ocupar toda a largura da tela
+    },
+  },
+
+  accordionContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '600px',
+    overflowY: 'hidden',
+    '@media (max-width: 1000px)': {
+      // Estilos para telas menores que 600px
+      width: '100%', // Definindo a largura como 100% para ocupar toda a largura da tela
+    },
+    '@media (max-width: 600px)': {
+      // Estilos para telas menores que 600px
+      width: '100%', // Definindo a largura como 100% para ocupar toda a largura da tela
+    },
+  },
+
+  accordionText: {
     span: {
       color: orange[800],
     },
