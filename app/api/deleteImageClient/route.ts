25f92db8a873486f5130a177819dbbe7fd57b12c
Caio Espindola <caioespindola@hotmail.com>
2024-08-27 08:29:33 -0300

    resolve bug that duplicate folders in local storage

---
+++ app/api/deleteImageClient/route.ts
@@ -0,0 +1,29 @@
+/* import { NextRequest, NextResponse } from 'next/server';
+import { del } from '@vercel/blob';
+
+export async function DELETE(request: NextRequest) {
+  const clientId = request.nextUrl.searchParams.get('clientId');
+
+  if (!clientId) {
+    return NextResponse.json(
+      { error: 'Client ID not provided' },
+      { status: 400 },
+    );
+  }
+
+  const fileName = `clients/${clientId}/profile-image.jpg`;
+
+  try {
+    // Deletar a imagem existente
+    await del(fileName);
+
+    return NextResponse.json({ message: 'Image deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting file:', error);
+    return NextResponse.json(
+      { error: 'An error occurred during the file deletion' },
+      { status: 500 },
+    );
+  }
+}
+ */
--- app/api/registerImageClient/route.ts
+++ app/api/registerImageClient/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+/* import { NextRequest, NextResponse } from 'next/server';
 import { put } from '@vercel/blob';
 import { clients } from '@/app/db/schema';
 import { db } from '@/app/db';
@@ -37,3 +37,4 @@ export async function POST(request: NextRequest) {
     );
   }
 }
+ */
