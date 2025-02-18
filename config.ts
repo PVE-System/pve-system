3644ea821b16006a113f506a04e87d8f67194a70
Caio Espindola <caioespindola@hotmail.com>
2025-02-18 09:17:17 -0300

    bug db

--- config.ts
+++ config.ts
@@ -1,10 +1,9 @@
 export const POSTGRES_URL =
-  process.env.POSTGRES_URL ||
-  'postgres://default:tOY84eilgZnz@ep-morning-feather-a4gma43p-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require';
-export const POSTGRES_USER = process.env.POSTGRES_USER || 'default';
+  process.env.POSTGRES_URL || 'postgres://postgres:postgres@localhost:5432';
+export const POSTGRES_USER = process.env.POSTGRES_USER || 'postgres';
 export const POSTGRES_HOST = process.env.POSTGRES_HOST || 'localhost';
-export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || '';
+export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || 'postgres';
 export const DB_PORT = process.env.DB_PORT
   ? parseInt(process.env.DB_PORT, 10)
   : 5432;
-export const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE || 'verceldb';
+export const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE || 'postgres';
