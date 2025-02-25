bbc9ccfa6ce9e866e3cdd7bdecf25bf344d57cf8
Caio Espindola <caioespindola@hotmail.com>
2025-02-25 19:22:44 -0300

    restart migration

--- db/migrations/0013_add_excel_page_tab_viewed_at.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE tabs_viewed
-ADD COLUMN excel_page_tab_viewed_at TIMESTAMP DEFAULT NOW();
--- db/migrations/0020_add_col_industry_in_salesQuotes_table.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE sales_quotes ADD COLUMN industry TEXT;
-
-ALTER TABLE sales_quotes ALTER COLUMN industry SET NOT NULL;
-
-ALTER TABLE sales_quotes ADD COLUMN quote_number INTEGER NOT NULL;
