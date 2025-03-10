dfca073e0422f3e3aab3a172cf10c3eab411281a
Caio Espindola <caioespindola@hotmail.com>
2025-03-10 10:10:34 -0300

    feat businessGroup Changes

--- app/api/checkIfGroupNameExists/route.ts
+++ app/api/checkIfGroupNameExists/route.ts
@@ -1,31 +1,27 @@
-// pages/api/checkIfGroupNameExists.ts
 import { db } from '@/app/db';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Método não permitido' });
-  }
-
-  const { name } = req.query; // Extrai o parâmetro "name" da query string
+// Apenas aceita requisições GET
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const name = searchParams.get('name'); // Obtém o parâmetro "name"
 
   if (!name) {
-    return res.status(400).json({ error: 'Nome não fornecido' });
+    return NextResponse.json({ error: 'Nome não fornecido' }, { status: 400 });
   }
 
   try {
     // Verifica se o nome já existe no banco de dados
     const group = await db.query.businessGroups.findFirst({
-      where: (group, { eq }) => eq(group.name, name as string),
+      where: (group, { eq }) => eq(group.name, name),
     });
 
-    // Retorna true se o nome já existir, false caso contrário
-    res.status(200).json({ exists: !!group });
+    return NextResponse.json({ exists: !!group }, { status: 200 });
   } catch (error) {
     console.error('Erro ao verificar nome do grupo:', error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
+    return NextResponse.json(
+      { error: 'Erro interno do servidor' },
+      { status: 500 },
+    );
   }
 }
