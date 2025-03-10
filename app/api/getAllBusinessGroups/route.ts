48e59d0842e7d23c680b9951a0417a24d2b1d79d
Caio Espindola <caioespindola@hotmail.com>
2025-03-10 10:31:27 -0300

    feat businessGroup Changes debugBuild

--- app/api/getAllBusinessGroups/route.ts
+++ app/api/getAllBusinessGroups/route.ts
@@ -1,20 +1,17 @@
 import { db } from '@/app/db';
 import { businessGroups, BusinessGroup } from '@/app/db/schema';
 import { NextRequest, NextResponse } from 'next/server';
-import { revalidateTag } from 'next/cache'; // Importar a função de revalidação
 
 export async function GET(request: NextRequest) {
   try {
-    // Buscar todos os grupos empresariais no banco de dados
     const allBusinessGroups: BusinessGroup[] = await db
       .select()
       .from(businessGroups);
 
-    // Revalidar a tag 'businessGroups' para garantir que o cache seja atualizado
-    revalidateTag('businessGroups');
-
-    // Criar a resposta JSON com a lista de grupos empresariais
-    return NextResponse.json({ businessGroups: allBusinessGroups });
+    return NextResponse.json(
+      { businessGroups: allBusinessGroups },
+      { status: 200, headers: { 'Cache-Control': 'no-store' } }, // Evita cache
+    );
   } catch (error) {
     console.error('Erro ao buscar grupos empresariais:', error);
     return NextResponse.json(
