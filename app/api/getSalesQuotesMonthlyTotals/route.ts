5a6a11510e357e7f6eed36da2d8013779db42e76
Caio Espindola <caioespindola@hotmail.com>
2025-11-04 09:34:45 -0300

    include graphic in client page tab quotes dynami route

--- app/api/getSalesQuotesMonthlyTotals/route.ts
+++ app/api/getSalesQuotesMonthlyTotals/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextRequest, NextResponse } from 'next/server';
 import { db } from '@/app/db/db';
 import { salesQuotes } from '@/app/db/schema';
